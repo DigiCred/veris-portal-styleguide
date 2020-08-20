e7c727f451edb7fccb7135d7c8248796095e9449
veris-sandeep <sandeep.babbar@veris.in>
2020-08-20 20:50:27 +0530

    [Fix] Multiline Text

--- src/components/textarea/index.js
+++ src/components/textarea/index.js
@@ -6,7 +6,9 @@ import Error from '../error'
 const Textarea = React.memo((props)=>{
     return (
         <React.Fragment>
-            <Label>{props.label}</Label>
+            {this.props.label && 
+                <Label>{`${this.props.label} ${this.props.isRequired?"*":""}`}</Label>
+            }
             <textarea className="vrs-textarea" 
                 style={{
                     border: props.error?"1px solid #f86f50": "1px solid #c2c5cc",
